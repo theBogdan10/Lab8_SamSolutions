6908020060527a380db2373c3ca202152ebe0d80
Bogdan <bybogdan@mail.ru>
2019-11-07 13:05:03 +0300

    4 task

---
+++ cookieSingleton.js
@@ -0,0 +1,34 @@
+const getCookieStr = name => (document.cookie.match(new RegExp(`${name}=\\S+;?`)) || [null])[0];
+const getCookieValue = str => str.split('=')[1];
+const deleteCookie = name => document.cookie = name + '=; Max-Age=-1;'; 
+
+const cookieSingleton = {
+    cookie: {
+
+    },
+    getCookie(name){
+        if(this.cookie[name]){
+            return this.cookie[name]
+        }
+        const cookieStr = getCookieStr(name);
+        if(cookieStr){
+            const cookieValue = getCookieValue(cookieStr);
+            this.cookie[name] = cookieValue;
+            return cookieValue
+        }
+        throw new Error('Nothing was found');
+    },
+    setCookie(name, value){
+        if(typeof name !== 'string' || name === ''){
+            throw new Error('Only strings are allowed')
+        }
+        this.cookie[name] = value;
+        document.cookie = `${name}=${value};`
+    },
+    deleteCookie(name){
+        delete this.cookie[name];
+        deleteCookie(name);
+    }
+}
+
+export default cookieSingleton
--- script.js
+++ script.js
@@ -1,17 +1,66 @@
-// 3 task
+// 4 task
 
-const button=document.getElementById("rem_adds")
-const adds=document.getElementById("adds")
+import cookieSingleton from './cookieSingleton.js';
 
-const isCookieExsist = cookie => 
-    !!(document.cookie.match(new RegExp(cookie)));
+const documentCookies = document.getElementById('documentCookies');
+const singletonCookies = document.getElementById('singletonCookies');
+const getCookieBtn = document.getElementById('getCookieBtn');
+const setCookieBtn = document.getElementById('setCookieBtn');
+const deleteCookieBtn = document.getElementById('deleteCookieBtn');
+const info = document.getElementById('info')
 
-setInterval(() => {
-    isCookieExsist('timeCookie') ?
-        adds.classList.add('hidden') :
-        adds.classList.remove('hidden')
-}, 200)
+const fillDocumentCookies = () => {
+    documentCookies.innerHTML = '';
+    document.cookie.split(';').forEach(
+        cookie => {
+            const li = document.createElement('li');
+            li.innerHTML = cookie;
+            documentCookies.appendChild(li);
+        }
+    )
+}
 
-button.onclick = () => {
-    document.cookie = 'timeCookie=""; max-age=3600';
+const fillSingletonCookies = () => {
+    singletonCookies.innerHTML = '';
+    for(let name in cookieSingleton.cookie){
+        const li = document.createElement('li');
+        li.innerHTML = `${name}=${cookieSingleton.cookie[name]}`;
+        singletonCookies.appendChild(li)
+    }
+}
+
+const fillInfo = infoText => info.innerHTML = infoText;
+
+fillDocumentCookies();
+fillSingletonCookies();
+
+getCookieBtn.onclick = e => {
+    const name = e.target.previousElementSibling.value + '';
+    try{
+        fillInfo(cookieSingleton.getCookie(name));
+    }
+    catch(e){
+        fillInfo(e)
+    }
+    
+}
+
+setCookieBtn.onclick = e => {
+    const value = e.target.previousElementSibling.value + '';
+    const name = e.target.previousElementSibling.previousElementSibling.value + '';
+    try{
+        cookieSingleton.setCookie(name, value);
+    }
+    catch(e){
+        fillInfo(e);
+    }
+    fillDocumentCookies();
+    fillSingletonCookies();
+}
+
+deleteCookieBtn.onclick = e => {
+    const name = e.target.previousElementSibling.value + '';
+    cookieSingleton.deleteCookie(name);
+    fillDocumentCookies();
+    fillSingletonCookies();
 }
